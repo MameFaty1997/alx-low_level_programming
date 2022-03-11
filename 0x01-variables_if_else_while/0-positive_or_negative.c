4e3db06bee4b1dd5437a3bce90bf49938ac49dca
MameFaty1997 <mamefaty1997@gmail.com>
2022-03-11 07:04:05 -0800

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if ( n > 0){
+		printf("%d is positive",n);
+	}
+	else if ( n = 0){
+		printf("%d is zero",n);
+	}
+	else
+		print("%d is negative",n);
 	return (0);
 }
