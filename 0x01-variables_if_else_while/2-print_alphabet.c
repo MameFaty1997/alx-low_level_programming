e13fe22b9ac4a3bc75e3a175f5548db61df1fa90
MameFaty1997 <mamefaty1997@gmail.com>
2022-03-12 09:08:51 -0800

    forst commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Print the alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
